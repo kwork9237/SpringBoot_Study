83bd1a6244e46e4875fabfe233e124410d8b2cdf
kwork9237 <kwork9237@gmail.com>
2023-04-05 22:34:52 +0900

    04/05 week5

--- src/main/java/com/co/kr/controller/FileListController.java
+++ src/main/java/com/co/kr/controller/FileListController.java
@@ -1,6 +1,10 @@
 package com.co.kr.controller;
 
 import java.io.IOException;
+import java.nio.file.DirectoryNotEmptyException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.ParseException;
 import java.util.HashMap;
 import java.util.List;
@@ -9,14 +13,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.co.kr.code.Code;
 import com.co.kr.domain.BoardFileDomain;
 import com.co.kr.domain.BoardListDomain;
+import com.co.kr.exception.RequestException;
 import com.co.kr.service.UploadService;
 import com.co.kr.vo.FileListVO;
 
@@ -62,6 +71,103 @@ public class FileListController {
 		mav.addObject("files", fileList);
 		
 		session.setAttribute("files", fileList);
+		
+		return mav;
+	}
+	
+	@GetMapping("detail")
+	public ModelAndView bdDetail(@ModelAttribute("fileListVO") FileListVO fileListVO, @RequestParam("bdSeq") String bdSeq, HttpServletRequest request) throws IOException {
+		ModelAndView mav = new ModelAndView();
+		
+		mav = bdSelectOneCall(fileListVO, bdSeq, request);
+		mav.setViewName("board/boardList.html");
+		
+		return mav;
+	}
+	
+	@GetMapping("edit")
+	public ModelAndView edit(FileListVO fileListVO, @RequestParam("bdSeq") String bdSeq, HttpServletRequest request) throws IOException {
+		ModelAndView mav = new ModelAndView();
+		
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		HttpSession session = request.getSession();
+		
+		map.put("bdSeq", Integer.parseInt(bdSeq));
+		BoardListDomain boardListDomain = uploadService.boardSelectOne(map);
+		List<BoardFileDomain> fileList = uploadService.boardSelectOneFile(map);
+		
+		fileListVO.setSeq(boardListDomain.getBdSeq());
+		fileListVO.setContent(boardListDomain.getBdContent());
+		fileListVO.setTitle(boardListDomain.getBdTitle());
+		fileListVO.setIsEdit("edit");
+		
+		mav.addObject("detail", boardListDomain);
+		mav.addObject("files", fileList);
+		mav.addObject("fileLen", fileList.size());
+		
+		mav.setViewName("board/boardEditList.html");
+		return mav;
+	}
+	
+	@PostMapping("editSave")
+	public ModelAndView editSave(@ModelAttribute("fileListVO") FileListVO fileListVO, MultipartHttpServletRequest request, HttpServletRequest httpReq) throws IOException {
+		ModelAndView mav = new ModelAndView();
+		
+		uploadService.fileProcess(fileListVO, request, httpReq);
+		
+		mav = bdSelectOneCall(fileListVO, fileListVO.getSeq(), request);
+		
+		fileListVO.setContent("");
+		fileListVO.setTitle("");
+		
+		mav.setViewName("board/boardList.html");
+		
+		return mav;
+	}
+	
+	@GetMapping("remove")
+	public ModelAndView mbRemove(@RequestParam("bdSeq") String bdSeq, HttpServletRequest request) throws IOException {
+		ModelAndView mav = new ModelAndView();
+		
+		HttpSession session = request.getSession();
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		List<BoardFileDomain> fileList = null;
+		
+		if(session.getAttribute("files") != null)
+			fileList = (List<BoardFileDomain>) session.getAttribute("files");
+		
+		map.put("bdSeq", Integer.parseInt(bdSeq));
+		
+		uploadService.bdContentRemove(map);
+		
+		for(BoardFileDomain list : fileList) {
+			list.getUpFilePath();
+			Path filePath = Paths.get(list.getUpFilePath());
+			
+			try {
+				Files.deleteIfExists(filePath);
+				uploadService.bdFileRemove(list);
+			}
+			catch(DirectoryNotEmptyException e) {
+				throw RequestException.fire(Code.E404, "디렉토리가 존재하지 않습니다.", HttpStatus.NOT_FOUND);
+			}
+			catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		session.removeAttribute("files");
+		mav = bdListCall();
+		mav.setViewName("board/boardList.html");
+		
+		return mav;
+	}
+	
+	public ModelAndView bdListCall() {
+		ModelAndView mav = new ModelAndView();
+		
+		List<BoardListDomain> items = uploadService.boardList();
+		mav.addObject("items", items);
 		
 		return mav;
 	}
---
+++ src/main/java/com/co/kr/vo/SigninVO.java
@@ -0,0 +1,13 @@
+package com.co.kr.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@AllArgsConstructor
+public class SigninVO {
+	private String id;	
+	private String pw;
+}
