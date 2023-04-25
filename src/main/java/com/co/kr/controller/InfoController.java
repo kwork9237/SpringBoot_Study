7a63d417c10e7fee1fadaebadfefbde62c3a5670
kwork9237 <kwork9237@gmail.com>
2023-04-25 23:06:33 +0900

    backup

--- src/main/java/com/co/kr/controller/InfoController.java
+++ src/main/java/com/co/kr/controller/InfoController.java
@@ -3,11 +3,15 @@ package com.co.kr.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.co.kr.domain.InfoDomain;
 import com.co.kr.service.InfoService;
 import com.co.kr.vo.SigninVO;
 
@@ -17,9 +21,22 @@ public class InfoController {
 	private InfoService infoService;
 	
 	@RequestMapping(value = "myPage")
-	public ModelAndView myPage() {
+	public ModelAndView myPage(HttpServletRequest req) {
 		ModelAndView mav = new ModelAndView();
 		
+		//세션에서 id 가져오기
+		HttpSession session = req.getSession();
+		String uid = (String) session.getAttribute("id");
+		
+		//개인 유저정보 가져오기
+		Map <String, String> map = new HashMap<>();
+		map.put("mbId", uid);
+		System.out.println(uid);
+		
+		InfoDomain id = infoService.infoSelect(map);
+		System.out.println(id);
+		
+		mav.addObject("item", id);
 		mav.setViewName("information/infoList.html");
 		
 		return mav;
